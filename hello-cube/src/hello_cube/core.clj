d46aa659653e4ad134f847c895e9c5aebe0281e1
Tim <TimothyStiles@users.noreply.github.com>
2016-04-25 16:46:32 -0400

    Update core.clj

--- hello-cube/src/hello_cube/core.clj
+++ hello-cube/src/hello_cube/core.clj
@@ -22,3 +22,10 @@
 ;uncomment and evaluate in repl to get a 3D printable cube spit into this
 ;project directory!.
 ;(main)
+
+;If you want to learn more than how to generate a simple cube then I definitely recommend checking out the overview
+;section at https://github.com/thi-ng/geom/tree/develop which shows a list of tutorials to help you get started
+;using the geom library. It's a great set of tools that is quite frankly far too huge to accurately start covering
+;here, but if you want to start generating your own meshes you can also check out this project:
+; https://github.com/TimothyStiles/hexer/blob/master/src/hexer/core.clj
+;that I wrote to generate hexagonal terrain to give you an idea of what you can do with geom in an afternoon.
